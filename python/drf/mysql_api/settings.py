d20ff45ba53656a5369bac5a2e2885700b52112e
takuto-oono <takutoono053037@gmail.com>
2025-05-05 23:08:52 +0900

    feat: mysqlの設定

--- python/drf/mysql_api/settings.py
+++ python/drf/mysql_api/settings.py
@@ -97,8 +97,16 @@ WSGI_APPLICATION = 'mysql_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'mysql_api'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
