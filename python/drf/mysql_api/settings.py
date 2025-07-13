311dd060bf6bc13b8661e5042f27b1d324e44142
takuto-oono <takutoono053037@gmail.com>
2025-07-13 19:44:41 +0900

    feat: prodの設定を追加

--- python/drf/mysql_api/settings.py
+++ python/drf/mysql_api/settings.py
@@ -151,3 +151,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
