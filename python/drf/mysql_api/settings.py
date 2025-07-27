610940501fe1a46bb22c6cf742ff03e10dc4f8ea
takuto-oono <takutoono053037@gmail.com>
2025-07-27 12:01:28 +0900

    fix: 本番環境のAdmin画面にCSSが効かない問題の修正

--- python/drf/mysql_api/settings.py
+++ python/drf/mysql_api/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,7 +146,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# 本番環境での静的ファイル配信設定
+if not DEBUG:
+    # WhiteNoiseを使用して静的ファイルを配信
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
